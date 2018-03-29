05ec2c93d9d600bce2c1247e522eb1d16491dad8
刘小安 <liuxiaoan8008@126.com>
2018-03-29 17:10:13 +0800

    add docstring for jieba_tokenizer, add unit test case for normalize.py

--- sklearn_plus/preprocessing/text/zh/jieba_tokenizer.py
+++ sklearn_plus/preprocessing/text/zh/jieba_tokenizer.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""This module is used for preprocessing the chinese sentence.  It contains
+ JiebaTokenizer class which used for segmenting chinese sentence into words.
+
+"""
+
 import jieba
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
 class JiebaTokenizer(BaseEstimator, TransformerMixin):
+    """Transforms chinese sentence to words by jieba.
+
+    This JiebaTokenizer transforms chinese sentence to words by jieba package.
+    You can add user-defined words to fine-tuning the segment result
+    when new a instance.
+
+    Parameters
+    ----------
+    words : array_like, default=None
+        user-defined words.
+
+    Attributes
+    ----------
+    tokenizer : Tokenizer
+        a jieba tokenizer object.
+
+    """
 
     def __init__(self, words=None):
         self.tokenizer = jieba.Tokenizer()
@@ -23,4 +46,11 @@ class JiebaTokenizer(BaseEstimator, TransformerMixin):
         return self.transform(X)
 
     def transform(self, X, y=None):
+        """Segemnting chinese sentence into words by jieba.
+
+        Parameters
+        ----------
+        X : array-like
+            Input data that will be transformed.
+        """
         return [u' '.join(self.tokenizer.lcut(x)) for x in X]
