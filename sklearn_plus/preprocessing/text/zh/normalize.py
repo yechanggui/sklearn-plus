ac28ced41b9e106da80152852e4f8e4192124e4b
刘小安 <liuxiaoan8008@126.com>
2018-04-11 13:20:10 +0800

    fix python 2,3 compatibility problem

--- sklearn_plus/preprocessing/text/zh/normalize.py
+++ sklearn_plus/preprocessing/text/zh/normalize.py
@@ -30,17 +30,5 @@ class Normalizer(BaseEstimator, TransformerMixin):
         return new_X
 
 
-test_str = '详情查看：http://www.ruyi.ai/，或者关注微信公众号“艾如意宝宝”获取更多信息' # utf-8
-print(list(test_str))
-print(type(test_str).__mro__)
-print(list(test_str.decode('utf-8'))) # utf-8 to unicode
-test_str2 = u'详情查看：http://www.ruyi.ai/，或者关注微信公众号“艾如意宝宝”获取更多信息' # unicode
-print(list(test_str2))
-
-normalizer = Normalizer()
-normalizer.transform(
-            [u'详情查看：http://www.ruyi.ai/，或者关注微信公众号“艾如意宝宝”获取更多信息']
-        )
-
 
 
