574405d3b88475ce0ce11275200a5defabb13d23
刘小安 <liuxiaoan8008@126.com>
2018-04-11 16:35:34 +0800

    fix package import problem

--- sklearn_plus/__init__.py
+++ sklearn_plus/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from .neural_network import *
 
 
-"""Top-level package for Sklearn Plus."""
-
-__author__ = """Yuwen Yan"""
-__email__ = 'ybbaigo@gmail.com'
+__author__ = """Yuwen Yan, Xiaoan Liu"""
+__email__ = 'ybbaigo@gmail.com, liuxiaoan@126.com'
 __version__ = '0.1.0'
-
