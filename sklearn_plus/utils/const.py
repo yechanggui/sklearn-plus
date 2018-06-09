7fdfc73d0166d6d9decc83c7e226aaccd77c3a93
Yuwen Yan <ybbaigo@gmail.com>
2018-06-09 23:10:57 +0800

    convert has_key to in, according to https://stackoverflow.com/questions/1323410/has-key-or-in

--- sklearn_plus/utils/const.py
+++ sklearn_plus/utils/const.py
@@ -2,7 +2,7 @@
 class _const:
     class ConstError(TypeError): pass
     def __setattr__(self,name,value):
-        if self.__dict__.has_key(name):
+        if name in self.__dict__:
             raise self.ConstError("Can't rebind const(%s)" % name)
         self.__dict__[name]=value
 import sys
