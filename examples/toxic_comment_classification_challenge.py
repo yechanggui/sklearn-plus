7969eff418c32696a479b8786eb501f96e946c0e
Yuwen Yan <ybbaigo@gmail.com>
2018-06-12 00:35:00 +0800

    fix #9

--- examples/toxic_comment_classification_challenge.py
+++ examples/toxic_comment_classification_challenge.py
@@ -4,15 +4,8 @@
 import logging
 import pandas as pd
 
-from sklearn_plus.preprocessing.bag_of_words import TextToBagVec
-from sklearn_plus.preprocessing.label_onehot_encoder import LabelOneHotEncoder
 from sklearn_plus.natural_language_processing.text_classification import Estimator as TextClassifier
 from sklearn_plus.natural_language_processing.text_classification.model_fn import text_cnn
-
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
 
 import tensorflow as tf
 
@@ -28,8 +21,16 @@ if __name__ == '__main__':
     print('raw data is ready...')
 
     params = tf.contrib.training.HParams(
+                filter_sizes = [1,2,3,4,5,6,7],
+                embed_dim = 100,
+                num_filters = 256,
+                dropout_keep_prob = 0.5,
+                l2_lambda = 0.0001,
+                decay_steps = 6000,
+                decay_rate = 0.65,
                 batch_size = 20,
                 num_epochs = 1,
+                learning_rate = 0.01,
                 max_document_length = 20
             )
  
